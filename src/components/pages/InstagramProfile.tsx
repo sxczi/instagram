a50c352ab115a571a3fb0f15e0293352987905ee
dogeduaa <quzabubaker@gmail.com>
2021-05-26 01:48:05 +0300

    profile page done

--- src/components/pages/InstagramProfile.tsx
+++ src/components/pages/InstagramProfile.tsx
@@ -4,7 +4,27 @@ class InstagramProfile extends React.Component {
     render() {
         return (
             <div className="InstagramProfile">
-                <h1>Profile</h1>
+                <div className="cont">
+                    <div className="left">
+                        <img src="/pfp.jpg" draggable="false" alt="" />
+                    </div>
+                    <div className="right">
+                        <div className="right-header">
+                            <h1>User</h1>
+                            <button>Edit Profile</button>
+                            <svg aria-label="Options" fill="#262626" height="24" viewBox="0 0 48 48" width="24"><path clip-rule="evenodd" d="M46.7 20.6l-2.1-1.1c-.4-.2-.7-.5-.8-1-.5-1.6-1.1-3.2-1.9-4.7-.2-.4-.3-.8-.1-1.2l.8-2.3c.2-.5 0-1.1-.4-1.5l-2.9-2.9c-.4-.4-1-.5-1.5-.4l-2.3.8c-.4.1-.8.1-1.2-.1-1.4-.8-3-1.5-4.6-1.9-.4-.1-.8-.4-1-.8l-1.1-2.2c-.3-.5-.8-.8-1.3-.8h-4.1c-.6 0-1.1.3-1.3.8l-1.1 2.2c-.2.4-.5.7-1 .8-1.6.5-3.2 1.1-4.6 1.9-.4.2-.8.3-1.2.1l-2.3-.8c-.5-.2-1.1 0-1.5.4L5.9 8.8c-.4.4-.5 1-.4 1.5l.8 2.3c.1.4.1.8-.1 1.2-.8 1.5-1.5 3-1.9 4.7-.1.4-.4.8-.8 1l-2.1 1.1c-.5.3-.8.8-.8 1.3V26c0 .6.3 1.1.8 1.3l2.1 1.1c.4.2.7.5.8 1 .5 1.6 1.1 3.2 1.9 4.7.2.4.3.8.1 1.2l-.8 2.3c-.2.5 0 1.1.4 1.5L8.8 42c.4.4 1 .5 1.5.4l2.3-.8c.4-.1.8-.1 1.2.1 1.4.8 3 1.5 4.6 1.9.4.1.8.4 1 .8l1.1 2.2c.3.5.8.8 1.3.8h4.1c.6 0 1.1-.3 1.3-.8l1.1-2.2c.2-.4.5-.7 1-.8 1.6-.5 3.2-1.1 4.6-1.9.4-.2.8-.3 1.2-.1l2.3.8c.5.2 1.1 0 1.5-.4l2.9-2.9c.4-.4.5-1 .4-1.5l-.8-2.3c-.1-.4-.1-.8.1-1.2.8-1.5 1.5-3 1.9-4.7.1-.4.4-.8.8-1l2.1-1.1c.5-.3.8-.8.8-1.3v-4.1c.4-.5.1-1.1-.4-1.3zM24 41.5c-9.7 0-17.5-7.8-17.5-17.5S14.3 6.5 24 6.5 41.5 14.3 41.5 24 33.7 41.5 24 41.5z" fill-rule="evenodd"></path></svg>
+                        </div>
+                        <div className="followers-bar">
+                            <p><strong>0</strong> posts</p>
+                            <p><strong>90</strong> followers</p>
+                            <p><strong>40</strong> following</p>
+                        </div>
+                        <div className="bio">
+                            <h1>User</h1>
+                            <p>I love TypeScript!</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
