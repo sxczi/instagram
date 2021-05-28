49edab034b1a4b35934786117101f46d15ee54e0
dogeduaa <quzabubaker@gmail.com>
2021-05-29 01:04:48 +0300

    f

--- src/components/pages/InstagramHomePage.tsx
+++ src/components/pages/InstagramHomePage.tsx
@@ -33,7 +33,7 @@ class InstagramHomePage extends React.Component<{}, HomePageState> {
                 username: "",
                 location: "",
                 postImageUrl: "",
-                pfpUrl: "/pfp.jpg",
+                pfpUrl: "",
                 description: "",
                 comments: [
                     {
@@ -48,7 +48,7 @@ class InstagramHomePage extends React.Component<{}, HomePageState> {
         stories: [
             {
                 username: "",
-                pfpUrl: "/pfp.jpg",
+                pfpUrl: "",
                 storyNew: false
             }
         ]
@@ -61,7 +61,8 @@ class InstagramHomePage extends React.Component<{}, HomePageState> {
         const file2 = await fetch('/data/stories.json');
         const stories = await file2.json();
 
-        this.setState({ posts, stories });
+        this.setState({ posts, stories  });
+        console.log(this.state)
     }
 
     render() {
