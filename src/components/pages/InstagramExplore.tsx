6603abcf4ded59dc98e52754a8d899a95c10f4e0
dogeduaa <quzabubaker@gmail.com>
2021-05-30 03:31:10 +0300

    fixed the security issues and added an under construction h3 in the xplore page

--- src/components/pages/InstagramExplore.tsx
+++ src/components/pages/InstagramExplore.tsx
@@ -5,6 +5,7 @@ class InstagramExplore extends React.Component {
     render() {
         return (
             <div className="InstagramExplore">
+                <h3>Under Construction</h3>
                 <Footer/>
             </div>
         )
